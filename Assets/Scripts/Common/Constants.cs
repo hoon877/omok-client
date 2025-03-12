14d473bf2049c2820e02dafe168465688e33d83d
hynje <hjyoon573@gmail.com>
2025-03-12 16:00:16 +0900

    SCRUM-12 입력처리
    마우스 클릭 좌표를 보드 좌표로 변환하고 position selector 표시

---
+++ Assets/Scripts/Common/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public const int BoardSize = 15;
+    public const float GridAreaRatio = 0.91f; // 전체 크기 대비 격자 영역의 비율, 632 / 692 * 100
+}
