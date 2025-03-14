6ae84ddc0f565983f88e38632a42da47b9fbc6c4
CodingsSin <cresend01@naver.com>
2025-03-14 18:04:01 +0900

    Slider Algorithm
    버그있음

--- Assets/Scenes/Wook/Scripts/GradeSystem.cs
+++ Assets/Scenes/Wook/Scripts/GradeSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,82 +8,48 @@ using UnityEngine.UI;
 
 public class GradeSystem : MonoBehaviour
 {
-    public Button btnWin, btnLose;
+    public Button btnWin, btnLose, btnReset;
     public Slider sdGrade;
     public TextMeshProUGUI txtGrade, txtState;
 
-    private int grade;
+    private int grade, score;
 
-    void Start()
+    private void Awake()
     {
-        btnWin.onClick.AddListener(Win);
-        btnLose.onClick.AddListener(Lose);
-
         if (PlayerPrefs.HasKey("Grade"))
         {
             grade = PlayerPrefs.GetInt("Grade");
+            score = PlayerPrefs.GetInt("Score");
+            MaxValueCheck();
         }
         else
         {
             grade = 18;
         }
         
-        FloatingGrade();
+        GradeView();
+    }
+    
+    void Start()
+    {
+        btnWin.onClick.AddListener(Win);
+        btnLose.onClick.AddListener(Lose);
+        btnReset.onClick.AddListener(Reset);
     }
 
-    void FloatingGrade()
+    void Reset()
+    {
+        grade = 18;
+        score = 30;
+        GradeView();
+    }
+
+    void GradeView()
     {
         txtGrade.text = "Your Grade : " + grade;
-        PlayerPrefs.SetInt("Grade", grade);
-    }
-
-    void Win()
-    {
-        txtState.text = "YOU WIN";
-        sdGrade.value += 10;
-
-        if (sdGrade.value >= sdGrade.maxValue)
-        {
-            if (grade == 1)
-            {
-                txtState.text = "Your Grade is Max";
-            }
-            else
-            {
-                txtState.text = "Your Grade is Up";
-                grade--;
-                sdGrade.value = 30;
-            }
-            FloatingGrade();
-        }
-        ColorCheck();
-    }
-
-    void Lose()
-    {
-        txtState.text = "YOU LOSE";
-        sdGrade.value -= 10;
+        sdGrade.value = score;
         
-        if (sdGrade.value <= 0)
-        {
-            if (grade == 18)
-            {
-                txtState.text = "Your Grade is Min";
-            }
-            else
-            {
-                txtState.text = "Your Grade is Down";
-                grade++;
-                sdGrade.value = 30;
-            }
-            FloatingGrade();
-        }
-        
-        ColorCheck();
-    }
-
-    void ColorCheck()
-    {
+        // Color
         if (sdGrade.value > 30)
         {
             sdGrade.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.blue;
@@ -94,6 +61,89 @@ public class GradeSystem : MonoBehaviour
         else
         {
             sdGrade.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.green;
+        }
+        
+        // Save UserInfo
+        PlayerPrefs.SetInt("Grade", grade);
+        PlayerPrefs.SetInt("Score", (int)sdGrade.value);
+    }
+    
+    void MaxValueCheck()
+    {
+        switch (grade)
+        {
+            case 1 :
+            case 2 :
+            case 3 :
+            case 4 :
+                sdGrade.maxValue = 130;
+                break;
+            case 5 :
+            case 6 :
+            case 7 :
+            case 8 :
+            case 9 :
+                sdGrade.maxValue = 80;
+                break;
+            default :
+                sdGrade.maxValue = 60;
+                break;
+        }
+    }
+
+    void Win()
+    {
+        txtState.text = "YOU WIN";
+        sdGrade.value += 10;
+
+        if (sdGrade.value >= sdGrade.maxValue)
+        {
+            UpGrade();
+        }
+
+        GradeView();
+    }
+
+    void UpGrade()
+    {
+        if (grade == 1)
+        {
+            txtState.text = "Your Grade is Max";
+        }
+        else
+        {
+            txtState.text = "Your Grade is Up";
+            grade--;
+            MaxValueCheck();
+            sdGrade.value = 30;
+        }
+    }
+
+    void Lose()
+    {
+        txtState.text = "YOU LOSE";
+        sdGrade.value -= 10;
+        
+        if (sdGrade.value <= 0)
+        {
+            DownGrade();
+        }
+        
+        GradeView();
+    }
+
+    void DownGrade()
+    {
+        if (grade == 18)
+        {
+            txtState.text = "Your Grade is Min";
+        }
+        else
+        {
+            txtState.text = "Your Grade is Down";
+            grade++;
+            MaxValueCheck();
+            sdGrade.value = sdGrade.maxValue - 30;
         }
     }
 }
