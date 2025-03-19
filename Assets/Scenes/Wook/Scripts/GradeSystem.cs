15d2e4706ead5cdb2e6c758bee728bd9995912c9
CodingsSin <cresend01@naver.com>
2025-03-19 11:16:47 +0900

    Slider Devide Line
    보조선 추가

--- Assets/Scenes/Wook/Scripts/GradeSystem.cs
+++ Assets/Scenes/Wook/Scripts/GradeSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,19 +11,21 @@ public class GradeSystem : MonoBehaviour
 {
     public Button btnWin, btnLose, btnReset;
     public Slider sdGrade;
+    public GameObject line;
     public TextMeshProUGUI txtGrade, txtState;
 
-    private int grade, score;
+    private int grade, score, div;
+    private float bar;
 
     private void Awake()
     {
+        bar = sdGrade.GetComponent<RectTransform>().rect.width;
+        
         if (PlayerPrefs.HasKey("Grade"))
         {
             grade = PlayerPrefs.GetInt("Grade");
             score = PlayerPrefs.GetInt("Score");
-            Debug.Log(sdGrade.maxValue);
             MaxValueSet();
-            Debug.Log(sdGrade.maxValue);
         }
         else
         {
@@ -43,6 +46,7 @@ public class GradeSystem : MonoBehaviour
     {
         grade = 18;
         score = 30;
+        MaxValueSet();
         GradeView();
     }
 
@@ -72,33 +76,53 @@ public class GradeSystem : MonoBehaviour
     
     void MaxValueSet()
     {
+        if (GameObject.FindWithTag("Lines"))
+        {
+            for (int i = 1; i < div; i++)
+            {
+                Destroy(GameObject.Find("Line" + i));
+            }
+        }
+        
         switch (grade)
         {
             case 1 :
             case 2 :
             case 3 :
             case 4 :
-                sdGrade.maxValue = 130;
+                div = 12;
                 break;
             case 5 :
             case 6 :
             case 7 :
             case 8 :
             case 9 :
-                sdGrade.maxValue = 80;
+                div = 8;
                 break;
             default :
-                sdGrade.maxValue = 60;
+                div = 6;
                 break;
+        }
+
+        sdGrade.maxValue = div * 10;
+        
+        // DevideLine
+        for (int i = 1; i < div; i++)
+        {
+            Vector2 pos = new Vector2((i - div / 2) * bar / (div * 192), sdGrade.transform.position.y);
+            GameObject obj = Instantiate(line, pos, Quaternion.identity, transform.Find("/Canvas/UI").transform);
+            obj.name = "Line" + i;
         }
     }
 
     void Win()
     {
         txtState.text = "YOU WIN";
+        
         sdGrade.value += 10;
         score += 10;
-        if (score == (int)sdGrade.maxValue)
+        
+        if (score >= (int)sdGrade.maxValue)
         {
             UpGrade();
         }
@@ -111,6 +135,7 @@ public class GradeSystem : MonoBehaviour
         if (grade == 1)
         {
             txtState.text = "Your Grade is Max";
+            score = (int)sdGrade.maxValue;
         }
         else
         {
@@ -124,10 +149,11 @@ public class GradeSystem : MonoBehaviour
     void Lose()
     {
         txtState.text = "YOU LOSE";
+        
         sdGrade.value -= 10;
         score -= 10;
         
-        if (score == 0)
+        if (score <= 0)
         {
             DownGrade();
         }
@@ -140,6 +166,7 @@ public class GradeSystem : MonoBehaviour
         if (grade == 18)
         {
             txtState.text = "Your Grade is Min";
+            score = 0;
         }
         else
         {
