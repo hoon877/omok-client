a977c58aedbbc401f0269062239de3dc8ec56212
phw0251 <phw0251@naver.com>
2025-03-27 18:27:46 +0900

    상품 생성 및 변수 할당

---
+++ Assets/Scripts/Shop/ProductPrefab.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductPrefab : MonoBehaviour
+{
+    public TMP_Text productAmountText;
+    public TMP_Text productPriceText;
+    public Image productImage;
+    public Button buyButton;
+
+    public void Setup(Product product, System.Action onBuy)
+    {
+        // UI에 상품 데이터 반영
+        productAmountText.text = $"{(product.coinAmount/1000).ToString()}K";
+        productPriceText.text = $"{product.coinPrice.ToString()}krw";
+        productImage.sprite = product.coinSprite;
+
+        // 구매 버튼에 이벤트 등록
+        buyButton.onClick.RemoveAllListeners();
+        buyButton.onClick.AddListener(() => onBuy?.Invoke());
+    }
+}
---
+++ Assets/Scripts/Shop/ShopPanel.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class ShopPanel : MonoBehaviour
+{
+    public Transform _productsPanel;
+}
